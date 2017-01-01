dbadc72255c8d485b3ff063d5f2ebf45cfa71840
EladCohenKaltura <EladCohenKaltura@users.noreply.github.com>
2017-01-01 08:10:27 +0200

    Add files via upload

--- Hackathon/2017/algo.js
+++ Hackathon/2017/algo.js
@@ -1,5 +1,20 @@
 var _cuepoints; 
 
-function fillCuePoints(kdp){
+function fillCuePoints(kdp)
+{
 	 _cuepoints = kdp.evaluate('{mediaProxy.entryCuePoints}');
+}
+
+// kdp.sendNotification("doPause")
+function bindKdpToEvenets(kdp, div)
+{
+	kdp.kBind("cuePointReached", function(){
+		div.innerHTML = div.innerHTML + 'Cue point reached ';
+	});
+	kdp.kBind("adOpportunity", function(){
+		div.innerHTML = div.innerHTML + 'ad opportunity';
+	});
+	kdp.kBind("adEnd", function(){
+		div.innerHTML = div.innerHTML + 'ad end';
+	});
 }
