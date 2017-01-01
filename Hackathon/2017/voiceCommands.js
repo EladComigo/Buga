5f34555354cb9aa9beffee22c41bd395a03cf84b
EladCohenKaltura <elad.cohen@kaltura.com>
2017-01-01 17:34:42 +0200

    additional voice commands

--- Hackathon/2017/voiceCommands.js
+++ Hackathon/2017/voiceCommands.js
@@ -1,34 +1,56 @@
-<!-- voice commands using the annyang library --> 
+<!-- voice commands using the annyang library -->
 
 if (annyang) {
-  // Let's define our first command. First the text we expect, and then the function it should call
-  
-  
-  var commands = {
-    'turn on the lights': function() {
-      document.getElementById('myImage').src='pic_bulbon.gif'
-    }, 
-	'turn off the lights': function() {
-	  document.getElementById('myImage').src='pic_bulboff.gif'
-    },
-	'play the video': function() {
-	  document.getElementById('myImage').src='pic_bulred.gif'
-	  window.kdp.sendNotification("doPlay");
-	},
-	'shoot': function() {
-		document.getElementById('myImage').src='pic_bulred.gif';
-		var div = document.getElementById('log');
-		div.innerHTML = div.innerHTML + JSON.stringify(window.kdp.evaluate('{mediaProxy.entryCuePoints}'));
-	}, 
-	'pause the video': function() {
-	  document.getElementById('myImage').src='pic_bulred.gif'
-	  window.kdp.sendNotification("doPause");
-	}
-  };
+    // Let's define our first command. First the text we expect, and then the function it should call
 
-  // Add our commands to annyang
-  annyang.addCommands(commands);
+    function setVoiceDivText(text)
+    {
+        document.getElementById('voiceDiv').innerText=text;
+    }
 
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
-  annyang.start();
+    function playTheVideo()
+    {
+        setVoiceDivText("play");
+        kdp.sendNotification("doPlay");
+    }
+    function pauseTheVideo()
+    {
+        setVoiceDivText("pause");
+        kdp.sendNotification("doPause");
+    }
+    function stopTheVideo()
+    {
+        setVoiceDivText("stop");
+        kdp.sendNotification("doStop");
+    }
+    function repeatLastSection()
+    {
+
+    }
+
+
+    var commands = {
+        /** play commands **/
+        'play the video': playTheVideo,
+        'play': playTheVideo,
+        'continue': playTheVideo,
+        /** pause commands **/
+        'pause the video': pauseTheVideo,
+        'pause': pauseTheVideo,
+        'wait': pauseTheVideo,
+        'halt': pauseTheVideo,
+        'just a sec': pauseTheVideo,
+        'just a second': pauseTheVideo,
+        /** stop **/
+        'stop': stopTheVideo,
+        'back to the start': stopTheVideo,
+        'go to start': stopTheVideo,
+        'back to top': stopTheVideo,
+    };
+
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
 }
