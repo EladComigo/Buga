9f894437c8ada72e733512e57787f5f577ba8afa
EladCohenKaltura <elad.cohen@kaltura.com>
2017-01-02 17:26:10 +0200

    hegwe

--- Hackathon/2017/algo.js
+++ Hackathon/2017/algo.js
@@ -1,61 +1,58 @@
-function bindKdpToEvenets(kdp, div)
+function chapterSeek(seekStartTime)
+{
+    kdp.sendNotification("doSeek", Math.round(seekStartTime/1000));
+    kdp.sendNotification("doPlay");
+    seekViaChapter = true;
+
+}
+
+function bindKdpToEvenets(kdp, chaptersDiv, logDiv)
 {
 
     kdp.kBind('cuePointsReceived', function ( cuePoints, id ) {
         cuepoints = cuePoints;
-        for (item in cuepoints) {
-                //div.innerHTML = div.innerHTML + 'updatedAt ' +  cuepoints[item][innerItem].updatedAt +  "<br />";
-                //div.innerHTML = div.innerHTML + 'cuePointType ' +  cuepoints[item][innerItem].cuePointType +  "<br />";
-                div.innerHTML = div.innerHTML + 'cuePointsReceived title ' +  cuepoints[item].title +  "<br />";
-                div.innerHTML = div.innerHTML + 'cuePointsReceived startTime ' +  cuepoints[item].startTime +  "<br />";
-                div.innerHTML = div.innerHTML + 'cuePointsReceived endTime ' +  cuepoints[item].endTime +  "<br />";
-                div.innerHTML = div.innerHTML + 'cuePointsReceived duration ' +  cuepoints[item].duration +  "<br />";
-                div.innerHTML = div.innerHTML + '------------------------------- '  +  "<br />";
-
+        for (var cuePoint in cuepoints)
+        {
+            logDiv.innerHTML = logDiv.innerHTML + 'cuePointsReceived title ' +  cuepoints[cuePoint].title +  "<br />";
+            logDiv.innerHTML = logDiv.innerHTML + 'cuePointsReceived startTime ' +  cuepoints[cuePoint].startTime +  "<br />";
+            logDiv.innerHTML = logDiv.innerHTML + 'cuePointsReceived endTime ' +  cuepoints[cuePoint].endTime +  "<br />";
+            logDiv.innerHTML = logDiv.innerHTML + 'cuePointsReceived duration ' +  cuepoints[cuePoint].duration +  "<br />";
+            logDiv.innerHTML = logDiv.innerHTML + '------------------------------- '  +  "<br />";
+            chaptersDiv.innerHTML = chaptersDiv.innerHTML + "<li onclick=\"chapterSeek(" + cuepoints[cuePoint].startTime + ")\">" + cuepoints[cuePoint].title  + "</li>"
         }
     });
-
-
 
     kdp.kBind("adOpportunity", function(cuePoint){
         seekToStart = cuePoint.cuePoint.startTime;
         seekToEnd = cuePoint.cuePoint.endTime;
 
-        div.innerHTML = div.innerHTML + 'adOpportunity start time '  +  cuePoint.cuePoint.startTime +   "<br />";
-        div.innerHTML = div.innerHTML + 'adOpportunity end time '    + cuePoint.cuePoint.endTime +   "<br />";
-
-
+        logDiv.innerHTML = logDiv.innerHTML + 'adOpportunity start time '  +  cuePoint.cuePoint.startTime +   "<br />";
+        logDiv.innerHTML = logDiv.innerHTML + 'adOpportunity end time '    + cuePoint.cuePoint.endTime +   "<br />";
     });
 
     kdp.kBind('playerUpdatePlayhead', function( currentTime ){
+        if (seekViaChapter)
+            return;
         var timeMiliSec  = currentTime * 1000;
-        var roundTime = Math.round(timeMiliSec);
-        var item, innerItem;
 
-        for (item in cuepoints) {
-				var start_time = cuepoints[item].startTime;
-                var end_time = cuepoints[item].endTime;
-				//div.innerHTML = div.innerHTML + "xxx " + roundTime + "/" +  end_time +  "<br />";
+        for (var cuePoint in cuepoints) {
+            var end_time = cuepoints[cuePoint].endTime;
 
-				if (Math.abs(roundTime - end_time) <= 30) {
-                    div.innerHTML = div.innerHTML + 'CuePointEnded = ' + cuepoints[item].title + " - " + roundTime + "/" + end_time  +  "<br />";
-                    this.sendNotification("doPause");
-					var src = cuepoints[item].sourceUrl;
-                    
-                    var img = $("<img  src=" + src + " id=step_img" + " style='position: absolute; top: 0; left: 0; width: 100%; height:100%; z-index: 10000000000; cursor: pointer; display: inline-block; '>")
-						.on('click', function(e){
-							$(this).remove();
-							kdp.sendNotification("doPlay");
-						});
-					$(this).append(img);
-                    break;
-                }
+            if (Math.abs(timeMiliSec - end_time) <= 30) {
+                logDiv.innerHTML = logDiv.innerHTML + 'CuePointEnded = ' + cuepoints[cuePoint].title + " - " + timeMiliSec + "/" + end_time  +  "<br />";
+                kdp.sendNotification("doPause");
+                var src = cuepoints[cuePoint].sourceUrl;
+
+                var img = $("<img  src=" + src + " id=step_img" + " style='position: absolute; top: 0; left: 0; width: 100%; height:100%; z-index: 10000000000; cursor: pointer; display: inline-block; '>")
+                    .on('click', function(e){
+                        $(this).remove();
+                        kdp.sendNotification("doPlay");
+                    });
+                $(this).append(img);
+                break;
             }
-
-
-
+        }
         //div.innerHTML = div.innerHTML + 'playerUpdatePlayhead ' + roundTime  +  "<br />";
-
     });
 
     kdp.kBind('KalturaSupport_ThumbCuePointsUpdated', function(e, cuepoints) {
@@ -67,6 +64,7 @@ function bindKdpToEvenets(kdp, div)
     });
 
     kdp.kBind('seeked', function () {
-        div.innerHTML = div.innerHTML + 'seeked' +  "<br />";
+        logDiv.innerHTML = logDiv.innerHTML + 'seeked' +  "<br />";
+        setTimeout(function(){seekViaChapter = false;}, 1000);
     });
 }
